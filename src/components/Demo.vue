282fecea542dc8cb1da3075c2885322e3f9c9b14
flybetter <flybetter@163.com>
2020-08-07 17:22:39 +0800

    add some vue feather like v-on v-bind

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -6,12 +6,48 @@
     </ul>
     <input type="text" v-model="name" placeholder="你的名字">
     <h1>你好,{{name}}</h1>
+    <h1>{{date}}</h1>
+    <span v-html="link"></span>
+    <div v-pre> {{name}}</div>
+    <div class="static" :class="{'active':isActive,'error':isError}"></div>
   </div>
 </template>
 
 <script>
+// var padDate=function (value) {
+//   return value<0?'0'+value:value;
+// }
+
 export default {
   name: 'Demo',
+  mounted () {
+    var _this = this
+    this.timer = setInterval(function () {
+      _this.date = new Date()
+    }, 1000)
+  },
+  beforeCreate () {
+    if (this.timer) {
+      clearInterval(this.timer)
+    }
+  },
+  // filter () {
+  //   formatDate: function f(value) {
+  //       var date=new Date(value);
+  //       var year=date.getFullYear();
+  //       var month=padDate(date.getMonth());
+  //       var day=padDate(date.getDay());
+  //       var hour=padDate(date.getHours());
+  //       var minutes=padDate(date.getMinutes());
+  //       var second=padDate(date.getSeconds());
+  //       return year+'-'+month+'-'+day+' '+hour+':'+minutes+':'+second;
+  //   }
+  // },
+  computed: {
+    now: function () {
+      return Date.now()
+    }
+  },
   data () {
     return {
       msg: 'i came from ',
@@ -20,7 +56,11 @@ export default {
         {name: 'demo2'},
         {name: 'demo3'}
       ],
-      name: ''
+      name: '',
+      date: new Date(),
+      link: '<a href="#">这个是一个链接</a>',
+      isActive: true,
+      isError: true
     }
   }
 }
